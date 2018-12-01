1b60be730139ac192bb36d8520bfb828f14d3f2c
sherlock_lin <sherlock_lin@163.com>
2018-12-01 16:43:58 +0800

    ttt

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,10 +1,13 @@
 package main;
 
-import charactor.*;
-import charactor.Hero;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class Test {
-	public static void main(String[] args) {
-		Hero hero = new Hero(5);
+	public static void main(String[] args) throws FileNotFoundException {
+		String pathname = "C:/Users/Administrator/Desktop/11.txt";
+		File file = new File(pathname);
+		new FileInputStream(file);
 	}
 }
