598c9323bb6464cff0500900cc55bed3d2e5704b
SherlockLHT <sherlock_lin@163.com>
2019-01-02 17:14:56 +0800

    finish database

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,17 +1,16 @@
 package main;
 
-import java.io.IOException;
-import file.FileOperator;
-import thread.*;
+import java.sql.SQLException;
+import database.DatabaseTest;
 
 public class Test {
-	public static void main(String[] args) throws IOException {
-		MyThread1 my_thread = new MyThread1();
-		//my_thread.start();
-		
-		MyThread2 my_thread2 = new MyThread2();
-		Thread thread = new Thread(my_thread2);
-		thread.start();
+	public static void main(String[] args) throws SQLException {
+		DatabaseTest database_test = new DatabaseTest();
+		//database_test.CreateTable();
+		database_test.InsertData();
+		//database_test.DeleteData();
+		database_test.SearchData2();
+		database_test.Disconnect();
 		
 		System.out.println("Finished");
 	}
