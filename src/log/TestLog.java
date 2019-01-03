d9ca4c7b265ea7995d1ebd020269febfe05e2b17
SherlockLHT <sherlock_lin@163.com>
2019-01-03 09:29:38 +0800

    update

--- src/log/TestLog.java
+++ src/log/TestLog.java
@@ -1,13 +1,13 @@
 package log;
 
-import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
 import org.apache.log4j.Logger;
 import org.apache.log4j.Level;
 
 public class TestLog {
 	public TestLog() throws InterruptedException {
 		Logger logger = Logger.getLogger(TestLog.class);
-		BasicConfigurator.configure();
+		PropertyConfigurator.configure("E:\\MyCode\\Java\\J2SE\\src\\log\\log.properties");
 		logger.setLevel(Level.DEBUG);
 		logger.trace("跟踪信息");
 		logger.debug("调试信息");
