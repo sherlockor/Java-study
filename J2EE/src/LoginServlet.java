7f69ab784acc34954f10a74d09456c7b35602a89
SherlockLHT <sherlock_lin@163.com>
2019-01-07 17:33:51 +0800

    update

--- J2EE/src/LoginServlet.java
+++ J2EE/src/LoginServlet.java
@@ -6,8 +6,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class LoginServlet extends HttpServlet {
+	public LoginServlet() {
+		System.out.println("Constractor");
+	}
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws IOException {
+		request.setCharacterEncoding("utf-8");
+		
 		String name = request.getParameter("name");
 		String password = request.getParameter("password");
 		System.out.println(name);
@@ -16,12 +21,12 @@ public class LoginServlet extends HttpServlet {
 		String html = null;
 		if (name.equals("admin") && password.equals("123")) {
 			System.out.println("Pass");
-			html = "<div style='color:green'>Success</div>";
+			html = "<div style='color:green'>³É¹¦</div>";
 		} else {
 			System.out.println("Fail");
-			html = "<div style='color:red'>Fail</div>";
+			html = "<div style='color:red'>Ê§°Ü</div>";
 		}
-		
+		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter pw = response.getWriter();
 		pw.println(html);
 	}
