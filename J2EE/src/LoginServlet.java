0fd32b42d61fd3e4bc8d9474fb7e5e7b315efe2f
SherlockLHT <sherlock_lin@163.com>
2019-01-08 11:25:52 +0800

    update

--- J2EE/src/LoginServlet.java
+++ J2EE/src/LoginServlet.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +11,7 @@ public class LoginServlet extends HttpServlet {
 		System.out.println("Constractor");
 	}
 	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws IOException {
+			HttpServletResponse response) throws IOException, ServletException {
 		request.setCharacterEncoding("utf-8");
 		
 		String name = request.getParameter("name");
@@ -18,7 +19,25 @@ public class LoginServlet extends HttpServlet {
 		System.out.println(name);
 		System.out.println(password);
 		
-		String html = null;
+		//服务端页面跳转,在服务端访问文件,并把文件内容发给浏览器
+		/*if (name.equals("admin") && password.equals("123")) {
+			System.out.println("Pass");
+			request.getRequestDispatcher("success.html").forward(request, response);
+		} else {
+			System.out.println("Fail");
+			request.getRequestDispatcher("fail.html").forward(request, response);
+		}*/
+		//客户端跳转
+		if (name.equals("admin") && password.equals("123")) {
+			System.out.println("Pass");
+			response.sendRedirect("success.html");
+		} else {
+			System.out.println("Fail");
+			response.sendRedirect("fail.html");
+		}
+		
+		//传回html
+		/*String html = null;
 		if (name.equals("admin") && password.equals("123")) {
 			System.out.println("Pass");
 			html = "<div style='color:green'>成功</div>";
@@ -28,6 +47,6 @@ public class LoginServlet extends HttpServlet {
 		}
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter pw = response.getWriter();
-		pw.println(html);
+		pw.println(html);*/
 	}
 }
