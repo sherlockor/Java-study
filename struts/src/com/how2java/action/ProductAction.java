fdd1c69ed60a427296d5e7b7d28d856323955602
SherlockLHT <sherlock_lin@163.com>
2019-01-21 16:51:06 +0800

    struct2中获取request和response

--- struts/src/com/how2java/action/ProductAction.java
+++ struts/src/com/how2java/action/ProductAction.java
@@ -1,11 +1,23 @@
 package com.how2java.action;
 
-import com.how2java.bean.Product;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class ProductAction {
+import org.apache.struts2.ServletActionContext;
+
+import com.how2java.bean.Product;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ProductAction extends ActionSupport {
 	private Product product;
 
 	public String show() {
+		HttpServletRequest request = ServletActionContext.getRequest();
+		HttpServletResponse response = ServletActionContext.getResponse();
+		
+		System.out.println(request);
+		System.out.println(response);
+		
 		product = new Product();
 		product.setName("iphone7");
 		return "show";
