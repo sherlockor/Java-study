0464ad06a41c54a6f8a4eba7d5b15d494d9ff48b
SherlockLHT <sherlock_lin@163.com>
2019-01-23 09:50:12 +0800

    update

---
+++ spring/src/com/how2java/pojo/Product.java
@@ -0,0 +1,28 @@
+package com.how2java.pojo;
+
+public class Product {
+	int id;
+	String name;
+	int price;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getId() {
+		return id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getPrice() {
+		return price;
+	}
+}
