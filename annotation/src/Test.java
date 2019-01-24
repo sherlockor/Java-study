2fac69e0852c6b20fe40340dc58293fa77bba844
SherlockLHT <sherlock_lin@163.com>
2019-01-24 09:00:09 +0800

    update

---
+++ annotation/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+	
+	public void out()
+	{
+		System.out.println("Test");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("aaa");
+		Class calssClass = null;
+		System.out.println("aaa");
+	}
+}
---
+++ annotation/src/TestA.java
@@ -0,0 +1,9 @@
+
+public class TestA extends Test{
+	
+	@Override
+	@SuppressWarnings(value = { "aa" })
+	public void out(){
+		System.out.println("Test A");
+	}
+}
