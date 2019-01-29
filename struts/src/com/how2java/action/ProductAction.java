9e2150f3f1ce0aff72475da57a9e24e970fad927
SherlockLHT <sherlock_lin@163.com>
2019-01-29 09:18:47 +0800

    update

--- struts/src/com/how2java/action/ProductAction.java
+++ struts/src/com/how2java/action/ProductAction.java
@@ -9,33 +9,33 @@ import com.how2java.bean.Product;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ProductAction extends ActionSupport {
-	private Product product;
+    private Product product;
 
-	public String show() {
-		HttpServletRequest request = ServletActionContext.getRequest();
-		HttpServletResponse response = ServletActionContext.getResponse();
+    public String show() {
+        HttpServletRequest request = ServletActionContext.getRequest();
+        HttpServletResponse response = ServletActionContext.getResponse();
 		
-		System.out.println(request);
-		System.out.println(response);
+        System.out.println(request);
+        System.out.println(response);
 		
-		product = new Product();
-		product.setName("iphone7");
-		return "show1";
-	}
-	public String add(){
+        product = new Product();
+        product.setName("iphone7");
+        return "show1";
+    }
+    public String add(){
         System.out.println("product.name:"+product.getName());
         return "show";
     }
-	public String age() {
-		System.out.println("product.name:"+product.getAge());
-		return "show";
-	}
+    public String age() {
+        System.out.println("product.name:"+product.getAge());
+        return "show";
+    }
 
-	public Product getProduct() {
-		return product;
-	}
+    public Product getProduct() {
+        return product;
+    }
 
-	public void setProduct(Product product) {
-		this.product = product;
-	}
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
