0a563e06d1091a734e8838e4aca82c120d85b038
SherlockLHT <sherlock_lin@163.com>
2019-02-14 17:26:37 +0800

    add mybatis

---
+++ mybatis/src/com/how2java/test/TestMybatis.java
@@ -0,0 +1,98 @@
+package com.how2java.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+import com.how2java.pojo.Category;
+import com.how2java.pojo.Product;
+
+public class TestMybatis {
+    String resource = "mybatis-config.xml";
+    //@Test
+    public void testCategory() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession(); 
+        
+        List<Category> categories = sqlSession.selectList("listCategory");
+        for(Category category : categories) {
+            System.out.println(category.getName());
+        }
+        sqlSession.close();
+    }
+    //@Test
+    public void testProduct() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession(); 
+        
+        List<Product> products = sqlSession.selectList("listProduct");
+        for(Product product : products) {
+            System.out.println(product.getName() + "  " + product.getPrice());
+        }
+        sqlSession.close();
+    }
+    //@Test
+    public void testAddCategory() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sessionFactory.openSession();
+        
+        Category category = new Category();
+        category.setName("new category");
+        sqlSession.insert("addCategory", category);
+        
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    //@Test
+    public void testDeleteCategory() throws IOException {
+        InputStream inputInputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputInputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        
+        Category category = new Category();
+        category.setName("new category111");
+        sqlSession.delete("deleteCategory", category);
+        
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    @Test
+    public void testSelectCategory() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        
+        Category category = new Category();
+        category.setId(2);
+        //List<Category> categories = sqlSession.selectList("getCategory", category);
+        List<Category> categories = sqlSession.selectList("getCategory");
+        
+        for (Category category2 : categories) {
+            System.out.println("select:" + category2.getName());
+        }
+        sqlSession.close();
+    }
+    //@Test
+    public void testUpdateCategory() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        
+        Category category = new Category();
+        category.setId(2);
+        category.setName("update category 2");
+        
+        sqlSession.update("updateCategory", category);
+        
+        sqlSession.commit();
+        sqlSession.close();
+    }
+}
