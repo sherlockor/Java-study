5daee286e90b6c50d099d6fd67c3ad0e8080876f
SherlockLHT <sherlock_lin@163.com>
2019-02-18 22:54:09 +0800

    update

---
+++ mybatis/src/com/how2java/pojo/Order.java
@@ -0,0 +1,28 @@
+package com.how2java.pojo;
+
+import java.util.List;
+
+public class Order {
+    private int id;
+    private String code;
+    private List<Product> products;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public List<Product> getProducts() {
+        return products;
+    }
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
