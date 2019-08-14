60505f93d398e2b0d7cff79da080cf095b4148b4
Sherlock <sherlock_lin@163.com>
2019-08-14 22:28:43 +0800

    update

--- study/src/main/java/com/myJava/study/web/Controller.java
+++ study/src/main/java/com/myJava/study/web/Controller.java
@@ -1,12 +1,21 @@
 package com.myJava.study.web;
 
+import java.text.DateFormat;
+import java.util.Date;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
     @RequestMapping("/hello")
-    public String hello() {
+    public String hello(Model model) throws Exception {
+        model.addAttribute("now", 
+                DateFormat.getDateTimeInstance().format(new Date()));
+        if(true) {
+            throw new Exception("Test Exception");
+        }
         return "Hello, World";
     }
 }
